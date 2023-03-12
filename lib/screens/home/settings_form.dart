49bee391f6422d87df4badb05dab54dd800f2303
Gavutham <kgavutham2005@gmail.com>
2023-03-13 00:04:11 +0530

    use stream builder in settings_form.dart to get the values of user data

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brew_coffee/models/user.dart';
+import 'package:brew_coffee/services/database.dart';
+import 'package:brew_coffee/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_coffee/shared/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({Key? key}) : super(key: key);
@@ -16,71 +20,84 @@ class _SettingsFormState extends State<SettingsForm> {
   //form values
   String _currentName = "";
   String _currentSugars = "";
-  int _currentStrength = 100;
+  int _currentStrength = 0;
 
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      autovalidateMode: AutovalidateMode.always,
-      child: Column(
-        children: [
-          Text(
-            "Update your brew settings.",
-            style: TextStyle(
-              fontSize: 18.0
+    final user = Provider.of<BrewUser>(context);
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          UserData? userData = snapshot.data;
+          return Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.always,
+            child: Column(
+              children: [
+                Text(
+                  "Update your brew settings.",
+                  style: TextStyle(
+                      fontSize: 18.0
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  initialValue: userData?.name,
+                  decoration: textInputDecoration,
+                  validator: (val) => val!.isEmpty ? "Please enter a value" : null,
+                  onChanged: (val) => setState(() => _currentName = val),
+                ),
+                SizedBox(height: 20.0,),
+                DropdownButtonFormField(
+                  decoration: textInputDecoration,
+                  value: _currentSugars == "" ? userData?.sugars : _currentSugars,
+                  items: sugars.map((sugar){
+                    return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('${sugar} sugars')
+                    );
+                  }).toList(),
+                  onChanged: (val) => _currentSugars = val.toString(),
+                ),
+                SizedBox(height: 20.0,),
+                Slider(
+                  value: _currentStrength == 0 ? userData!.strength.toDouble() : _currentStrength.toDouble(),
+                  activeColor: Colors.brown[_currentStrength],
+                  inactiveColor: Colors.white,
+                  min: 100,
+                  max: 900,
+                  divisions: 8,
+                  onChanged: (val) => setState(() {
+                    _currentStrength = val.toInt();
+                  }),
+                ),
+                SizedBox(height: 20.0,),
+                ElevatedButton(
+                  onPressed: () async {
+                    print(_currentName);
+                    print(_currentSugars);
+                    print(_currentStrength);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.pink[400]),
+                  ),
+                  child: Text(
+                    "Update",
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 20.0,),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val!.isEmpty ? "Please enter a value" : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20.0,),
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars == "" ? '0': _currentSugars,
-            items: sugars.map((sugar){
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('${sugar} sugars')
-              );
-            }).toList(),
-            onChanged: (val) => _currentSugars = val.toString(),
-          ),
-          SizedBox(height: 20.0,),
-          Slider(
-            value: _currentStrength.toDouble(),
-            activeColor: Colors.brown[_currentStrength],
-            inactiveColor: Colors.white,
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (val) => setState(() {
-              _currentStrength = val.toInt();
-            }),
-          ),
-          SizedBox(height: 20.0,),
-          ElevatedButton(
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(Colors.pink[400]),
-            ),
-            child: Text(
-              "Update",
-              style: TextStyle(
-                color: Colors.white
-              ),
-            ),
-          ),
-        ],
-      ),
+          );
+        }else{
+          return Loading();
+        }
+
+      }
     );
   }
 }
